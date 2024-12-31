0e751a2062ae276b16bb27b0c577992ffe752cce
gg307 <gg307@email.com>
2024-12-31 18:37:03 +0100

    add

--- app/app.h
+++ app/app.h
@@ -2,5 +2,35 @@
 
 namespace vsite::oop::v6 
 {
+		template<typename T>
+		struct point {
+			T a;
+		};
+
+		template<typename T>
+		T minimum(T a, T b) {
+			if (a < b) {
+				return a;
+			}
+			else {
+				return b;
+			}
+		}
+
+
+
+		template<typename T, int I>
+		class fixed_array {
+		private:
+			T arr[I];
+
+		public:
+			T operator[] (int a) {
+				return arr[a];
+			}
+			unsigned long long size() {
+				return I;
+			}
+		};
 
 }
